00ce9b2622fd567ae440f86aae593bfe29133d14
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-01-03 08:06:44 +0530

    last name validation

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
 package classProblemByAmolMateSir.Day23PrcticeProblem;
 
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // Scanner input = new Scanner(System.in);
         System.out.println("welcome to user registration");
-
-        boolean firstName = Pattern.matches("[A-Za-z]{0,8}",input.next());
+//firstName validation
+        boolean firstName = Pattern.matches("[A-Za-z]{0,8}", "Mayur");
         System.out.println(firstName);
 
-
+//lastName validation
+        boolean lastName = Pattern.matches("[A-Za-z]{0,8}", "Polojwar");
+        System.out.println(lastName);
     }
 }
